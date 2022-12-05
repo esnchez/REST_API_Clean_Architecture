a3f2f489f1f100a263e2b035eb66b1674ddacb0a
Eric <sanchez.eric@epitech.eu>
2022-12-05 17:52:46 +0100

    testing on post controller&route

--- tests/nomination.test.ts
+++ tests/nomination.test.ts
@@ -1,7 +1,19 @@
 import request from "supertest"; 
 import {App} from "../src/main/App";
+import {MemberNominationRequest} from "../src/context/Members/domain/entities/MemberNomination" 
 
 let app : App
+
+const nomination : MemberNominationRequest = {
+    emailRef: "testRef@test.com",
+    emailNom: "test2Nom@test.com",
+    description: "test_description",
+    communityScore: 5,
+    talentScore: 5
+}
+
+
+
 
 describe('Testing API REST', () => {
 
@@ -41,6 +53,20 @@ describe('Testing API REST', () => {
               });;
             
             expect(response.body).toEqual(expect.objectContaining({ nominations : expect.any(Array)}));
+        });
+
+    });
+
+    describe('POST /nomination', () => {
+
+        it('Controller call returns 500 status code on empty request', async () => {
+            const response = await request(app.httpServer).post('/nomination').send({});
+            expect(response.statusCode).toBe(500);
+        });
+
+        it('Controller returns 201 status code when submitting adequate request', async () => {
+            const response = await request(app.httpServer).post('/nomination').send(nomination);
+            expect(response.statusCode).toBe(201);
         });
 
     });
